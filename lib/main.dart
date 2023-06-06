153052bbb53a371cddcb61fc06c07af9d997a858
armaanpatel <armaan0427@gmail.com>
2023-06-05 23:48:10 -0500

    Added flutter support on MacOS

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,9 @@ import 'package:sklps_app/services/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:sklps_app/services/wrapper.dart';
 import 'package:flutter/services.dart';
-import 'package:sklps_app/shared/size_config.dart';
+import 'firebase_options.dart';
+
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,7 +16,7 @@ void main() async {
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
   runApp(const MyApp());
 }
 
